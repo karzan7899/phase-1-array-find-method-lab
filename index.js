8244dd7da309f03569a63c14fae57fb2574b7ad9
karzan <karzanluqman99@gmail.com>
2022-08-13 11:37:00 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+function superbowlWin(recc) {
+
+    function check(element, index, array) {
+        //if (element.result === "N/A") { return false; }
+        if (element.result === "W") { return true; }
+    }
+    //  console.log((recc.find(check).year))
+    return (recc.find(check).year)
+}
+
+
