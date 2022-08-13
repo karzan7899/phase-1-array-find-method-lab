2d389d53b7e6df70086caa8cebb6a4ad8f52456d
karzan <karzanluqman99@gmail.com>
2022-08-13 12:51:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-function superbowlWin(recc) {
+function superbowlWin(record) {
 
-    function check(element, index, array) {
-        //if (element.result === "N/A") { return false; }
-        if (element.result === "W") { return true; }
-    }
-    //  console.log((recc.find(check).year))
-    return (recc.find(check).year)
+    // function check(element, index, array) {
+    //     // if (element.result === "N/A") { return false; }
+    //     if (element.result === "W") { return (element.year); }
+    // }
+    // //  console.log((recc.find(check).year))
+    // return (recc.find(check))
+
+    const result = record.find((record) => record.result === 'W');
+    return result ? result.year : undefined;
+
 }
 
 
